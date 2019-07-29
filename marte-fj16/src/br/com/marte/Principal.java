63598a2e35212454a14b4231e69127a0d6e934f9
Luis Roth <vitlroth@gmail.com>
2019-07-29 10:44:54 -0300

    * Criada classe Principal

---
+++ marte-fj16/src/br/com/marte/Principal.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.marte;
+
+/**
+ * @author vitlr
+ *
+ */
+public class Principal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
